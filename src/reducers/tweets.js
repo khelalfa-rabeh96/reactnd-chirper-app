b2d7c42121817dd23d64a9c31a0c46f79ad06ea2
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-03-20 14:07:17 +0100

    style: remove ineeded space

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -3,10 +3,10 @@ import { RECEIVE_TWEETS } from '../actions/tweets'
 export default function users(state={}, action){
 	switch(action.type){
 		case RECEIVE_TWEETS:
-		return{
-			...state,
-			...action.tweets
-		}
+			return{
+				...state,
+				...action.tweets
+			}
 
 		default:
 			return state
